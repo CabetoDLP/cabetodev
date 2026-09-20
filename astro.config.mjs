7e25b2e09dd214aa05e25c76b9b0bde4938086b6
CabetoDLP <cabetodp@gmail.com>
2026-09-19 19:00:42 -0500

    remove node server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import sitemap from '@astrojs/sitemap';
-
-import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
+
   vite: {
     plugins: [tailwindcss()]
   },
 
-  i18n: {
-    defaultLocale: 'es',
-    locales: ['es', 'en'],
-    routing: {
-      prefixDefaultLocale: false // 'es' no lleva prefijo (/proyectos), 'en' sí (/en/proyectos)
-    }
-  },
-
-  // Ajusta este dominio por la URL final de tu portafolio de desarrollo
   site: 'https://cabetodev.onrender.com',
 
   integrations: [sitemap()],
-
-  adapter: node({
-    mode: 'standalone',
-  }),
 });
